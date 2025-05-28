37c75cc713b2b80d23417acb82815db0c2933130
rafael grando <sta_rafinha@hotmail.com>
2025-05-28 11:30:51 -0300

    fix: Adicionado arquivo sql V8 para versionamento da base de dados no modelo migration usando flyway

---
+++ src/main/resources/db/migration/V8__28-05-2025-db-belonging-and-seed.sql
@@ -0,0 +1,19 @@
+create table tb_belonging
+(
+    game_id  BIGSERIAL NOT NULL,
+    list_id  BIGSERIAL NOT NULL,
+    position INT,
+    CONSTRAINT fk_belonging_game_id_list_id PRIMARY KEY (game_id,list_id)
+);
+
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (1, 1, 0);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (1, 2, 1);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (1, 3, 2);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (1, 4, 3);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (1, 5, 4);
+
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (2, 6, 0);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (2, 7, 1);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (2, 8, 2);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (2, 9, 3);
+INSERT INTO tb_belonging (list_id, game_id, position) VALUES (2, 10, 4);
